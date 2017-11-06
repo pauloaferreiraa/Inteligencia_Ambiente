22fa8d89a6d56e643fa448ad75d7300f873d344d
pauloaferreiraa <pauloazevedoferreiraa@gmail.com>
2017-11-06 16:31:38 +0000

    Class Database C#

---
+++ AmI_Tp1/AmI_Tp1/Database.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace AmI_Tp1
+{
+    public class Database
+    {
+        private MySqlConnection connection;
+        private string database;
+        private MySqlCommand command;
+        public Database(string server, string database, string uid, string password)
+        {
+            this.database = database;
+            connection = new MySqlConnection("SERVER=" + server + ";DATABASE=" + database + ";UID=" + uid + ";PASSWORD=" + password);
+        }
+
+        public void insert(string query)
+        {
+            try
+            {
+                command = new MySqlCommand(query, connection);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public bool connect()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        //Close connection
+        public bool close()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
